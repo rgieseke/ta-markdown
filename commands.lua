5edb0f5b696503ca2d3bb887a0708cccff5740b5
rgieseke <none@none>
2010-06-25 15:50:21 +0200

    Updated header toggling, added word count.

--- commands.lua
+++ commands.lua
@@ -1,14 +1,12 @@
 -- Commands for the Markdown module.
 module('_m.markdown.commands', package.seeall)
 
-local textadept = _G.textadept
-local b = buffer
-
 -- Blockquotes
 local m_editing = _m.textadept.editing
 m_editing.comment_string.markdown = '> '
 
-function underline(char)
+local function underline(char)
+  local b = buffer
   b:begin_undo_action()
   b:line_end()
   caret = b.current_pos
@@ -21,24 +19,40 @@ function underline(char)
   b:end_undo_action()
 end
 
-function header(level)
+local function header(level)
+  local b = buffer
   local pos = b.current_pos
   b:begin_undo_action()
-  b:home()
-  buffer:add_text(string.rep('#', level)..' ')
-  b:goto_pos(pos + level + 1)
+  _m.textadept.editing.select_line()
+  sel = b:get_sel_text()
+  sel, count = sel:gsub('#+', string.rep('#', level))
+  if count == 0 then
+    b:home()
+    b:add_text(string.rep('#', level)..' ')
+    b:goto_pos(pos + level + 1)
+  else
+    b:replace_sel(sel)
+  end
   b:end_undo_action()
 end
 
-function enclose(char)
-  b:begin_undo_action()
-  local txt = b:get_sel_text()
-  if txt == '' then
-    b:word_left_extend()
-    txt = b:get_sel_text()
+local m_editing = _m.textadept.editing
+
+m_editing.enclosure.star = { left = '*', right = '*' }
+m_editing.enclosure.underline = { left = "_", right = "_" }
+
+local function word_count()
+  local buffer = buffer
+  local text, length = buffer:get_text(buffer.get_length)
+  if #text > 0 then text = text.." " end
+  text = string.gsub(text, "^%s+", "")
+  seps = string.gmatch(text, "%s+")
+  local count = 0
+  for i in seps do
+    count = count + 1
   end
-  b:replace_sel(char..txt..char)
-  b:end_undo_action()
+  status = 'Words: %d - Chars: %d'
+  gui.statusbar_text = status:format(count, buffer.length)
 end
 
 -- Markdown-specific key commands.
@@ -47,9 +61,9 @@ local keys = _G.keys
 if type(keys) == 'table' then
   keys.markdown = {
     al = {
-      m = { textadept.io.open,
-            textadept.iconv(_USERHOME..'/modules/markdown/init.lua',
-                            'UTF-8', _CHARSET) },
+      m = { io.open_file,
+          (_USERHOME..'/modules/markdown/init.lua'):iconv('UTF-8', _CHARSET) },
+      i = { word_count },
     },
     ['a='] = { underline, '=' },
     ['a-'] = { underline, '-' },
@@ -59,9 +73,9 @@ if type(keys) == 'table' then
     ['a4'] = { header, 4 },
     ['a5'] = { header, 5 },
     ['a6'] = { header, 6 },
-    ac = {
-      ['*'] = { enclose, '*' },
-      ['_'] = { enclose, '_' }
-    }
+    ac = { -- enclose in
+    ['*'] = { m_editing.enclose, 'star' },
+    ["_"] = { m_editing.enclose, 'underline' },
+    },
   }
 end
--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 -- Markdown module.
--- April 2010, Robert Gieseke
--- freely distributable under the same license as Textadept
+-- Robert Gieseke, robert.gieseke@gmail.com
+-- freely distributable under the same license as Textadept (MIT)
 
 module('_m.markdown', package.seeall)
 
@@ -8,7 +8,7 @@ if type(_G.snippets) == 'table' then
 -- Container for Markdown-specific snippets.
 -- @class table
 -- @name snippets.markdown
-  _G.snippets.markdown= {}
+  _G.snippets.markdown = {}
 end
 
 if type(_G.keys) == 'table' then
@@ -25,4 +25,5 @@ function set_buffer_properties()
   if not buffer.use_tabs then
     buffer.indent = 4
   end
+--  events.connect('update_ui', word_count, 1)
 end
