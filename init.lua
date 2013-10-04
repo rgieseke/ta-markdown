b917b08d506152139037a81ec6d0ccb1cf38eab3
Robert Gieseke <rob.g@web.de>
2013-10-04 12:35:45 +0200

    Documentation and short cut overhaul
    Run command is already in Textadept core.

--- init.lua
+++ init.lua
@@ -1,18 +1,18 @@
--- A markdown module for the
--- [Textadept](http://foicica.com/textadept/) editor.
--- It provides utilities for writing
+-- A Markdown module for the
+-- [Textadept](http://foicica.com/textadept/) editor with some short cuts and
+-- snippets for writing
 -- [Markdown](http://daringfireball.net/projects/markdown/).<br>
 --
 -- Installation:<br>
 -- Download an
 -- [archived](https://github.com/rgieseke/ta-markdown/archives/master)
 -- version or clone the git repository into your `.textadept` directory:
+--
 --     cd ~/.textadept/modules
 --     git clone https://github.com/rgieseke/ta-markdown.git \
 --         markdown
 --
---
--- The source is on [GitHub](https://github.com/rgieseke/ta-markdown),
+-- The [source](https://github.com/rgieseke/ta-markdown) is on GitHub,
 -- released under the
 -- [MIT license](http://www.opensource.org/licenses/mit-license.php).
 
@@ -22,14 +22,12 @@ local M = {}
 -- ## Settings
 
 -- Local variables.
-local m_editing, m_run = textadept.editing, textadept.run
+local editing, run = textadept.editing
 -- Blockquotes.
-m_editing.comment_string.markdown = '> '
--- Run command (using file extension).
-m_run.md = 'markdown %(filename)'
+editing.comment_string.markdown = '> '
 -- Auto-matching chars.<br>
 -- Match `<` for embedded HTML, don't match `'`.
-m_editing.char_matches.markdown = {
+editing.char_matches.markdown = {
   [40] = ')', [91] = ']', [123] = '}', [34] = '"', [60] = '>'
 }
 
@@ -67,7 +65,7 @@ function M.header(level)
   local b = buffer
   local pos = b.current_pos
   b:begin_undo_action()
-  m_editing.select_line()
+  editing.select_line()
   sel = b:get_sel_text()
   sel, count = sel:gsub('#+', string.rep('#', level))
   if count == 0 then
@@ -86,24 +84,12 @@ function M.remove_header()
   local b = buffer
   local pos = b.current_pos
   b:begin_undo_action()
-  m_editing.select_line()
+  editing.select_line()
   sel = b:get_sel_text()
   sel = sel:gsub('#+ ', '')
   b:replace_sel(sel)
   b:line_end()
   b:end_undo_action()
-end
-
--- Enclose selected text or insert char.
--- Parameter:<br>
--- _left_: Char to insert on the left.<br>
--- _right_: Char to insert on the right.
-function M.enclose_selection(left, right)
-  if buffer:get_sel_text() == '' then
-    return false
-  else
-    m_editing.enclose(left, right)
-  end
 end
 
 -- Display word and char count in status bar.
@@ -126,18 +112,18 @@ end
 
 -- Markdown-specific key commands.
 keys.markdown = {
-  al = {
-    -- __Alt-L, M__: Open this module for editing.
+  [keys.LANGUAGE_MODULE_PREFIX] = {
+    -- Open this module for editing: `Alt/⌘-L` `M`
     m = { io.open_file,
-        (_USERHOME..'/modules/markdown/init.lua'):iconv('UTF-8', _CHARSET) },
-    -- __Alt-L, I__: Display char and word count.
+        (_USERHOME..'/modules/markdown/init.lua') },
+    --  Show char and word count: `Alt/⌘-L` `I`
     i = { word_count },
   },
-  -- Underline current line: `Alt/⌘`+'='
+  -- Underline current line: `Alt/⌘ ='
   [OSX and 'm=' or 'a='] = { M.underline, '=' },
-  -- Underline current line: `Alt/⌘`+'-'
+  -- Underline current line: `Alt/⌘ -`
   [OSX and 'm-' or 'a-'] = { M.underline, '-' },
-  -- __Alt-0 - 6__: Change header level.
+  -- Change header level: `Alt/⌘-0 … 6`
   [OSX and 'm0' or 'a0'] = { M.remove_header },
   [OSX and 'm1' or 'a1'] = { M.header, 1 },
   [OSX and 'm2' or 'a2'] = { M.header, 2 },
@@ -145,16 +131,11 @@ keys.markdown = {
   [OSX and 'm4' or 'a4'] = { M.header, 4 },
   [OSX and 'm5' or 'a5'] = { M.header, 5 },
   [OSX and 'm6' or 'a6'] = { M.header, 6 },
-  -- __Alt-C, *__: Enclose in *.<br>
-  -- **Alt-C, _**: Enclose in _.
-  ac = { -- enclose in
-    ['*'] = { m_editing.enclose, '*', '*' },
-    ["_"] = { m_editing.enclose, '_', '_' },
-  },
-  -- Enclose selected text.
-  ["*"] = { M.enclose_selection, "*", "*" },
-  ['_'] = { M.enclose_selection, '_', '_' },
-  ['`'] = { M.enclose_selection, '`', '`' },
+  -- Enclose selected text or previous word:<br>
+  -- `Alt/⌘ *`, `Alt/⌘ _`, ``Ctrl Alt/⌘ ` ``
+  [OSX and 'm*' or 'a*'] = { editing.enclose, "*", "*" },
+  [OSX and 'm_' or 'a_'] = { editing.enclose, '_', '_' },
+  [OSX and 'cm`' or 'ca`'] = { editing.enclose, '`', '`' },
 }
 
 -- ## Snippets.
